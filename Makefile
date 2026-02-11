2205a7cc32bc119b6ccc1bed25fc24a34fdd9720
Anael Murtas <anael.muurtas@gmail.com>
2026-02-11 16:15:57 +0100

    first minishell push

---
+++ Makefile
@@ -0,0 +1,35 @@
+NAME        = minishell
+
+CC          = cc
+CFLAGS      = -Wall -Wextra -Werror -g
+
+SRCS        = srcs/minishell.c
+
+
+OBJ_DIR     = obj/
+
+OBJS        = $(SRCS:%.c=$(OBJ_DIR)%.o)
+
+LIBFT       	= ./includes/Libft/libft.a
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	make -C ./includes/Libft
+	$(CC) $(CFLAGS) $(OBJS) $(LIBFT) -o $(NAME)
+
+$(OBJ_DIR)%.o: %.c
+		@mkdir -p $(dir $@)
+		$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -rf $(OBJ_DIR)
+	make clean -C ./includes/Libft
+
+fclean: clean
+	rm -f $(NAME)
+	make clean -C ./includes/Libft
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ includes/minishell.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amurtas <amurtas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 15:52:15 by amurtas           #+#    #+#             */
+/*   Updated: 2026/02/11 16:10:55 by amurtas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+# include "Libft/libft.h"
+
+#endif
