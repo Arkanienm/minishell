7c3a8c9f6a32b85eb2d260a419427811db2ac34f
Anael Murtas <anael.muurtas@gmail.com>
2026-02-17 19:06:06 +0100

    advencing parsing

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,13 +6,42 @@
 /*   By: amurtas <amurtas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 15:51:20 by amurtas           #+#    #+#             */
-/*   Updated: 2026/02/13 15:14:21 by amurtas          ###   ########.fr       */
+/*   Updated: 2026/02/17 18:44:40 by amurtas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	tokenizer(char **)
+void	lexer(char *str, t_token *token)
+{
+	if (ft_strncmp(str, "|", ft_strlen(str)) == 0)
+		token->type = PIPE;
+	else if (ft_strncmp(str, "<", ft_strlen(str)) == 0)
+		token->type = REDIR_IN;
+	else if (ft_strncmp(str, ">", ft_strlen(str)) == 0)
+		token->type = REDIR_OUT;
+	else if (ft_strncmp(str, "<<", ft_strlen(str)) == 0)
+		token->type = HEREDOC;
+	else if (ft_strncmp(str, ">", ft_strlen(str)) == 0)
+		token->type = APPEND;
+	else
+		token->type = WORD;
+	token->content = str;
+}
+
+int	tokenizer(char **argv, t_token *token)
+{
+	int	i;
+	char	**args;
+	
+	i = 0;
+	while (argv[i])
+	{
+		lexer(argv[i], token);
+		token = token->next;
+		i++;
+	}
+}
 
 int	parsing(int argc, char **argv)
 {
