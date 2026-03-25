2558817311ec0140796e29aca3a9e0804eb8ccba
Anael Murtas <anael.muurtas@gmail.com>
2026-03-25 17:03:32 +0100

    norm

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -1,6 +1,6 @@
 #include "../includes/minishell.h"
 
-int		g_status = 0;
+int	g_status = 0;
 
 void	ft_free_data(t_envp_data *data)
 {
@@ -21,41 +21,41 @@ void	ft_free_data(t_envp_data *data)
 void	line_manage(char **line, t_envp_data *envp)
 {
 	if ((*line) == NULL)
-    {
-        free((*line));
-        ft_free_data(envp);
-        exit(0);
-    }
-    if ((*line))
-        add_history((*line));
+	{
+		free((*line));
+		ft_free_data(envp);
+		exit(0);
+	}
+	if ((*line))
+		add_history((*line));
 }
 
-int    minishell_loop(t_envp_data *envp)
+int	minishell_loop(t_envp_data *envp)
 {
-    t_token    *token;
-    t_cmd    *cmd;
-    char    *line;
+	t_token	*token;
+	t_cmd	*cmd;
+	char	*line;
 
-    while (1)
-    {
-        cmd = NULL;
-        line = readline("minishell> ");
-        line_manage(&line, envp);
-        token = tokenizer(line);
-        if (token)
-        {
-            expander(token, envp);
-            remove_quotes(token);
-            parser(token, &cmd);
-            if (cmd && cmd->cmd[0])
+	while (1)
+	{
+		cmd = NULL;
+		line = readline("minishell> ");
+		line_manage(&line, envp);
+		token = tokenizer(line);
+		if (token)
+		{
+			expander(token, envp);
+			remove_quotes(token);
+			parser(token, &cmd);
+			if (cmd && cmd->cmd[0])
 				g_status = pipex(envp, cmd);
-        }
-        ft_free_struct(token);
-        free_cmd_struct(cmd);
-        free(line);
-    }
-    if(g_status)
-        return g_status;
+		}
+		ft_free_struct(token);
+		free_cmd_struct(cmd);
+		free(line);
+	}
+	if (g_status)
+		return (g_status);
 }
 
 int	main(int argc, char **argv, char **envp)
