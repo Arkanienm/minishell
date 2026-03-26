cf99d75ab3408183fa8a9814faac4dada162bb6a
Anael Murtas <anael.muurtas@gmail.com>
2026-03-26 16:32:47 +0100

    norminette in all srcs files without subfiles

--- srcs/minishell_utils.c
+++ srcs/minishell_utils.c
@@ -72,13 +72,13 @@ void	free_cmd_struct(t_cmd *lst)
 void	write_redir_error(t_token *current)
 {
 	if (current->next->type == PIPE)
-		ft_putstr_fd("bash: syntax error near unexpected token `|'\n", 1);
+		ft_putstr_fd("bash: syntax error near unexpected token `|'\n", 2);
 	else if (current->next->type == REDIR_IN)
-		ft_putstr_fd("bash: syntax error near unexpected token `<'\n", 1);
+		ft_putstr_fd("bash: syntax error near unexpected token `<'\n", 2);
 	else if (current->next->type == REDIR_OUT)
-		ft_putstr_fd("bash: syntax error near unexpected token `>'\n", 1);
+		ft_putstr_fd("bash: syntax error near unexpected token `>'\n", 2);
 	else if (current->next->type == APPEND)
-		ft_putstr_fd("bash: syntax error near unexpected token `>>'\n", 1);
+		ft_putstr_fd("bash: syntax error near unexpected token `>>'\n", 2);
 	else if (current->next->type == HEREDOC)
-		ft_putstr_fd("bash: syntax error near unexpected token `<<'\n", 1);
+		ft_putstr_fd("bash: syntax error near unexpected token `<<'\n", 2);
 }
